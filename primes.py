04a19e18a3c57451975f970aa4db2dc35dde28d6
Gonçalo  Azevedo <zalosantos02@gmail.com>
2022-09-26 23:58:08 +0100

    Done primes

--- primes.py
+++ primes.py
@@ -3,4 +3,28 @@
 
 def primes(number_of_primes):
     list = []
+    semiList =[]
+    n = len(list)
+    i = 1
+    x = 1
+    while n < number_of_primes:
+        semiList.clear()
+        i=1
+        while i<=x:
+          if x % i== 0:
+             semiList.append(i)
+             i= i+1
+
+          else :
+             i=i+1
+        if len(semiList) == 2:
+            list.append(x)
+            n= n+1
+        else:
+            i=1
+        x=x+1
+
+
+
+
     return list

e0e2e301c578a43306dc6e7d1c0dc5cfc2666f76
Gonçalo  Azevedo <zalosantos02@gmail.com>
2022-09-27 00:05:03 +0100

    Now done for real

--- primes.py
+++ primes.py
@@ -7,23 +7,25 @@ def primes(number_of_primes):
     n = len(list)
     i = 1
     x = 1
-    while n < number_of_primes:
-        semiList.clear()
-        i=1
-        while i<=x:
-          if x % i== 0:
-             semiList.append(i)
-             i= i+1
-
-          else :
-             i=i+1
-        if len(semiList) == 2:
-            list.append(x)
-            n= n+1
-        else:
+    if(number_of_primes>0):
+        while n < number_of_primes:
+            semiList.clear()
             i=1
-        x=x+1
+            while i<=x:
+              if x % i== 0:
+                 semiList.append(i)
+                 i= i+1
 
+              else :
+                 i=i+1
+            if len(semiList) == 2:
+                list.append(x)
+                n= n+1
+            else:
+                i=1
+            x=x+1
+    else:
+        raise ValueError
 
 
 
